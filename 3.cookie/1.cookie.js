11642b1aecddc7f54d88b8c2b37e23b85769fd2d
Chongduo Yang <ycdchn@163.com>
2017-02-15 11:25:42 +0800

    Update 1.cookie.js

--- 3.cookie/1.cookie.js
+++ 3.cookie/1.cookie.js
@@ -4,7 +4,8 @@ http.createServer(function(req,res){
         //设置响应头中Set-Cookie,客户端可以读取到此响应头并将cookie保存在本地
         res.setHeader('Set-Cookie',['age=6','home=beijing']);
         res.end();
-    }else if(req.url == '/read'){//服务器读取cookie的时候
+    }else if(req.url == '/read'){//服务器读取cookie的时候 // 前段设置cookie的 是通过document.cookie 就可以了 
+        //浏览器会自动把我们cookie 放在cookies字段 
         var cookie = req.headers['cookie'];
         res.end(cookie);
     }else{
